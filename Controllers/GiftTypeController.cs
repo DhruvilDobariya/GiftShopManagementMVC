fa4589c1fe41b988fcab2f4bdeb5ef0d4c33069e
Dhruvil Dobariya <190540107051@darshan.ac.in>
2022-04-07 17:23:08 +0530

    Complete controller of Company and GiftType

---
+++ Controllers/GiftTypeController.cs
@@ -0,0 +1,91 @@
+﻿using GiftShopManagement.Insterfaces;
+using GiftShopManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GiftShopManagement.Controllers
+{
+    public class GiftTypeController : Controller
+    {
+        private readonly ICRUDRepository<GiftType> _crudRepository;
+
+        public GiftTypeController(ICRUDRepository<GiftType> crudRepository)
+        {
+            _crudRepository = crudRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _crudRepository.GetAllAsync());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(GiftType giftType)
+        {
+            if (ModelState.IsValid)
+            {
+                bool flag = await _crudRepository.InsertAsync(giftType);
+                if (flag)
+                {
+                    ModelState.Clear();
+                    TempData["Success"] = "giftType added successfully.";
+                    return View();
+                }
+            }
+            TempData["Error"] = _crudRepository.Message;
+            return View(giftType);
+        }
+
+        public async Task<IActionResult> Update(int Id)
+        {
+            if (Id == 0)
+            {
+                return NotFound("giftType Not Found.");
+            }
+            var giftType = await _crudRepository.GetByIdAsync(Id);
+            if (giftType == null)
+            {
+                return NotFound("giftType Not Found");
+            }
+            return View(giftType);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update(GiftType giftType)
+        {
+            if (ModelState.IsValid)
+            {
+                bool flag = await _crudRepository.UpdateAsync(giftType);
+                if (flag)
+                {
+                    TempData["Success"] = "giftType updated successfully";
+                    return RedirectToAction("Index");
+                }
+                TempData["Error"] = _crudRepository.Message;
+            }
+            return View(giftType);
+        }
+
+        public async Task<IActionResult> Delete(int Id)
+        {
+            if (Id == 0)
+            {
+                return NotFound("giftType Not Found.");
+            }
+            bool flag = await _crudRepository.DeleteAsync(Id);
+            if (flag)
+            {
+                TempData["Success"] = "giftType deleted successfully.";
+                return RedirectToAction("Index");
+            }
+            TempData["Error"] = _crudRepository.Message;
+            return RedirectToAction("Index");
+        }
+    }
+}
